c7748177bce280c50b93f704a8b67bf7a723d70a
Manoj984200 <manojsharma.kumar1989@gmail.com>
2026-07-29 21:53:06 +0530

    Add bcrypt dependency and Express updates

--- Express/models/user.model2.js
+++ Express/models/user.model2.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema2 = new mongoose.Schema({
+
     name: {
         type: String,
         required: [true, "Name is required"]
@@ -22,14 +24,31 @@ const userSchema2 = new mongoose.Schema({
         ]
     },
 
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+
     phoneNumber: {
         type: Number,
-        required: [true, "Phone Number is required"]
+        unique: true,
+        required: [true, "Phone Number is required"],
+        match: [/^[6-9]\d{9}$/, "Invalid Indian mobile number"]
     }
+
+});
+
+// This function runs automatically before saving a user
+userSchema2.pre("save", async function () {
+
+    console.log("Data is getting saved!!!!!");
+
+    this.password = await bcrypt.hash(this.password, 10);
+
 });
 
 const User2 = mongoose.model("User2", userSchema2);
 
 module.exports = {
-    User2
+    User2,
 };
